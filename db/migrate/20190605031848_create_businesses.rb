5d4f465417aed82be591748e8328cbbcdb3a8bdf
Irfan Maknojia <imak_7@yahoo.com>
2019-06-04 22:41:08 -0500

    add attributes to business

--- db/migrate/20190605031848_create_businesses.rb
+++ db/migrate/20190605031848_create_businesses.rb
@@ -1,7 +1,10 @@
 class CreateBusinesses < ActiveRecord::Migration[5.2]
   def change
     create_table :businesses do |t|
-
+      t.string :name
+      t.text :description
+      t.text :address
+      t.integer :price
       t.timestamps
     end
   end

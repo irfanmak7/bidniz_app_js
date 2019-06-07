02471e2db64b0964558ce245af525b80d6ee16d7
Irfan Maknojia <imak_7@yahoo.com>
2019-06-07 15:44:48 -0500

    update index method

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,6 +1,8 @@
 class BusinessesController < ApplicationController
 
     def index
+        @user = User.find_by(id: params[:user_id])
+        # @user ? @businesses = @user.businesses : page_not_found
     end
 
     def create

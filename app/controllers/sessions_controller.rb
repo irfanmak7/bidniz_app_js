a43952a392a8649b3ce97e437217660027748949
Irfan Maknojia <imak_7@yahoo.com>
2019-06-07 12:12:54 -0500

    add comments

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,10 +15,12 @@ class SessionsController < ApplicationController
         end
     end
 
+    # Login with Facebook
     def facebookAuth
         @user = User.from_omniauth(auth)
         @user.save
         session[:user_id] = @user.id
         redirect_to user_path(@user)
     end
+
 end

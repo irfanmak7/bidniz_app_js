6491bb5eedfc8dd2d186283006808ef35d6f49b6
Irfan Maknojia <imak_7@yahoo.com>
2019-06-06 21:26:53 -0500

    create a private method for user_params in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,5 +23,8 @@ class UsersController < ApplicationController
     end
 
     private
-    
+
+    def user_params
+        params.require(:user).permit(:name, :email, :phone_number, :password)
+    end
 end

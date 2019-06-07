23fa43d51522078b5ad90ed8945ff94046114d02
Irfan Maknojia <imak_7@yahoo.com>
2019-06-07 15:32:38 -0500

    add sub heading

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_07_041503) do
+ActiveRecord::Schema.define(version: 2019_06_05_031848) do
 
   create_table "businesses", force: :cascade do |t|
     t.string "name"
@@ -28,7 +28,6 @@ ActiveRecord::Schema.define(version: 2019_06_07_041503) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "uid"
   end
 
 end

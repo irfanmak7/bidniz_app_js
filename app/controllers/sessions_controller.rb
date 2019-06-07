2f633dc8f8301935d39c7f87a5d55c611fe5f60f
Irfan Maknojia <imak_7@yahoo.com>
2019-06-07 12:10:31 -0500

    Add flash message to create method and redirect to login_path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,15 @@ class SessionsController < ApplicationController
     def new
     end
 
+    # Normal Login
     def create
         @user = User.find_by(email: params[:email])
         if @user && @user.authenticate(params[:user][:password])
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
-            redirect_to '/login'
+            flash[:message] = "Please enter the correct information."
+            redirect_to login_path
         end
     end
 

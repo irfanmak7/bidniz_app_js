b0e9b9e61201b24e55fb3d3276ff50967336fd81
Irfan Maknojia <imak_7@yahoo.com>
2019-06-07 13:45:08 -0500

    add ':logged_in?' to line 3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
-    helper_method :current_user, :require_login
+    helper_method :current_user, :logged_in?, :require_login
 
     def current_user
         User.find_by(id: session[:user_id])

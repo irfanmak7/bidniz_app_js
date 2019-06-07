b72015c34e5a269417b7af0ad08ebce8a5d5fb74
Irfan Maknojia <imak_7@yahoo.com>
2019-06-06 21:23:02 -0500

    update create method in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,13 @@ class UsersController < ApplicationController
     end
 
     def create
+        @user = User.new(user_params)
+
+        if user.save
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
     end
 
     def edit

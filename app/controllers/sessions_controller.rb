6dff834b5fb55818c86917a2907880afc69b3ab7
Irfan Maknojia <imak_7@yahoo.com>
2019-06-06 23:20:00 -0500

    clean up blank lines

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,12 @@ class SessionsController < ApplicationController
     end
 
     def create
-        
         @user = User.find_by(email: params[:email])
-        if @user && @user.authenticate(params[:password])
+        if @user && @user.authenticate(params[:user][:password])
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
-            redirect_to login_path
+            redirect_to '/login'
         end
-        
     end
 end

54dd59fba9340cc5393202b3861e190e014489ad
Irfan Maknojia <imak_7@yahoo.com>
2019-06-08 15:51:19 -0500

    fixed user find by on line 14

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
 
     # Normal Login
     def create
-        @user = User.find_by(email: params[:email])
+        @user = User.find_by(email: params[:user][:email])
         if @user && @user.authenticate(params[:user][:password])
             session[:user_id] = @user.id
             flash[:success] = "Welcome #{@user.name}!"

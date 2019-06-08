6c925ad53ce121c917d2b7c3b751d43e30c5679d
Irfan Maknojia <imak_7@yahoo.com>
2019-06-08 12:07:00 -0500

    Move current_user and logged_in method to application helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,6 @@ class ApplicationController < ActionController::Base
 
     helper_method :current_user, :logged_in?, :require_login
 
-    def current_user
-        User.find_by(id: session[:user_id])
-    end
-
-    def logged_in?
-        session[:user_id]
-    end
-
     def require_login
         redirect_to root_path, alert: "You have to login to see this page." unless session.include? :user_id
     end

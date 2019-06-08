d96ab8b6c115ffff2a2e8f96c02a8ece6c28662a
Irfan Maknojia <imak_7@yahoo.com>
2019-06-08 12:19:57 -0500

    Add uid to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_07_205032) do
+ActiveRecord::Schema.define(version: 2019_06_08_171854) do
 
   create_table "businesses", force: :cascade do |t|
     t.string "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2019_06_07_205032) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uid"
   end
 
 end

a70922a0a50caa44778ad5f29148b05d78696cea
Irfan Maknojia <imak_7@yahoo.com>
2019-06-10 12:48:31 -0500

    add has_many through association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_many :businesses
+    has_many :comments, through: :businesses
     validates :email, presence: true
     validates :email, uniqueness: true
     has_secure_password

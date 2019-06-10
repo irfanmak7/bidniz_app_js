acd24732c741ee3c137c517efb1cad57ef8bf0bc
Irfan Maknojia <imak_7@yahoo.com>
2019-06-10 16:25:36 -0500

    add destroy method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
 
     def create
         @business = Business.find(params[:business_id])
-        @comment = Comment.create(params[:comment].permit[:content])
+        @comment = @business.comments.create(params[:comment].permit[:content])
         @comment.user_id = current_user.id
         @comment.business_id = @business.id
 
@@ -10,6 +10,10 @@ class CommentsController < ApplicationController
             redirect_to user_business_path(@business.user, @business)
         else
             render :new
+        end
+
+        def destroy
+
         end
     end
 end

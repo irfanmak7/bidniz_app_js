88ebe0d68f1340cc9c8400fa0a3303422484ecd0
Irfan Maknojia <imak_7@yahoo.com>
2019-06-10 16:09:18 -0500

    update comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,15 @@
 class CommentsController < ApplicationController
 
     def create
+        @business = Business.find(params[:business_id])
+        @comment = Comment.create(params[:comment].permit[:content])
+        @comment.user_id = current_user.id
+        @comment.business_id = @business.id
 
+        if @comment.save
+            redirect_to user_business_path(@business.user, @business)
+        else
+            render :new
+        end
     end
-
 end

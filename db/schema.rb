274e75f483b7ff32a212bcefdf694b999d3926f7
Irfan Maknojia <imak_7@yahoo.com>
2019-06-24 20:00:27 -0500

    ran migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_10_174020) do
+ActiveRecord::Schema.define(version: 2019_06_21_040833) do
 
   create_table "businesses", force: :cascade do |t|
     t.string "name"
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 2019_06_10_174020) do
     t.integer "user_id"
   end
 
+  create_table "businesses_tags", force: :cascade do |t|
+    t.integer "business_id"
+    t.integer "tag_id"
+  end
+
   create_table "comments", force: :cascade do |t|
     t.text "content"
     t.integer "business_id"
@@ -30,6 +35,10 @@ ActiveRecord::Schema.define(version: 2019_06_10_174020) do
     t.datetime "updated_at", null: false
     t.index ["business_id"], name: "index_comments_on_business_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|

0d7bea72c5950b89d005328c979f896541e5dc74
Irfan Maknojia <imak_7@yahoo.com>
2019-06-24 19:47:27 -0500

    add businesses_tags has_many association

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,4 +1,7 @@
 class Business < ApplicationRecord
     belongs_to :users, dependent: :destroy, required: false
     has_many :comments, dependent: :destroy
+
+    has_many :businesses_tags
+    has_many :tags, through: :businesses_tags
 end

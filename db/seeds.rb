53fe54bd7ce3e631bb13c7151a6d398f0f0fbd2b
Irfan Maknojia <imak_7@yahoo.com>
2019-08-14 13:58:17 -0500

    Create new users

---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+User.create(name: "Johnny Appleseed", email: "johnnyappleseed@test.com", phone_number: "2811234567", password: "test")
+User.create(name: "Jane Appleseed", email: "janeappleseed@test.com", phone_number: "2810987654", password: "test")

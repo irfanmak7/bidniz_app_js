8bda6e5e66a0f5203968a5b09740fa2a32f40d02
Irfan Maknojia <imak_7@yahoo.com>
2019-08-14 19:53:09 -0500

    create new js object and add html to dom

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -3,7 +3,9 @@ $(document).ready(function() {
         $("button#view-comments").on("click", function() {
             let id = $(this).data("id");
             $.get("/businesses/" + id + ".json", function(data) {
-
+                const mycomments = new twoBusiness(data)
+                const viewCommentsHTML = mycomments.showCommentsHTML()
+                document.getElementById('ajax-comment-data').innerHTML += viewCommentsHTML
             })
         })
     })

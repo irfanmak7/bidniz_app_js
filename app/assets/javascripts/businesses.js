79d0d2bfe3245ef2a3e299730a25aff36aa54faf
Irfan Maknojia <imak_7@yahoo.com>
2019-08-14 19:44:47 -0500

    get id from page and get json from businesses/:id.json

--- app/assets/javascripts/businesses.js
+++ app/assets/javascripts/businesses.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
     $(function() {
         $("button#businesses-more-info").on("click", function(){
-            
+            let id = $(this).data("id");
+            $.get("/businesses/" + id + ".json", function(data) {
+                
+            })
         })
     })
 })

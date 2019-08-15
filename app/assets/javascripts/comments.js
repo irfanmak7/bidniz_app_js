6c1716994397644525468965644cc6b49cbb8657
Irfan Maknojia <imak_7@yahoo.com>
2019-08-14 19:54:11 -0500

    create twoBusiness class

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -12,3 +12,13 @@ $(document).ready(function() {
     })
 });
 
+class twoBusiness {
+    constructor(obj) {
+        this.id = obj.id
+        this.name = obj.name
+        this.description = obj.description
+        this.address = obj.address
+        this.price = obj.price
+        this.comments = obj.comments
+    }
+}

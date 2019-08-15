bc782c956e4f3434e6b39db805c9402c5c4461e8
Irfan Maknojia <imak_7@yahoo.com>
2019-08-14 19:45:48 -0500

    create new business, set call to postAdditionalInfoFunction to variable

--- app/assets/javascripts/businesses.js
+++ app/assets/javascripts/businesses.js
@@ -3,7 +3,9 @@ $(document).ready(function() {
         $("button#businesses-more-info").on("click", function(){
             let id = $(this).data("id");
             $.get("/businesses/" + id + ".json", function(data) {
-                
+                const mybusiness1 = new oneBusiness(data)
+                const myBusiness1HTML = mybusiness1.postAdditionalInfoHTML()
+                document.getElementById('ajax-additional-info').innerHTML += myBusiness1HTML        
             })
         })
     })

a0f6b19fac93cb7d35f8f1ad9e9582c11551aae0
Irfan Maknojia <imak_7@yahoo.com>
2019-08-14 19:52:25 -0500

    get id from page and get json from page

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
     $(function() {
         $("button#view-comments").on("click", function() {
+            let id = $(this).data("id");
+            $.get("/businesses/" + id + ".json", function(data) {
 
+            })
         })
     })
 });
